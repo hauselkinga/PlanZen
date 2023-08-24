944d70ec2d13a6101876fedd370c94cab32a6fd0
Hausel Kinga <hausel.kinga@gmail.com>
2023-08-24 13:44:01 +0200

    create entities for database

---
+++ PlanZen-Backend/PlanZen.API/Entities/Column.cs
@@ -0,0 +1,24 @@
+﻿namespace PlanZen.API.Entities
+{
+    public class Column
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        public ICollection<Card> Cards { get; set;} = new List<Card>();
+
+        [ForeignKey("TableId")]
+        public Table? Table { get; set; }
+        public int TableId { get; set; }
+
+        public Column(string title)
+        {
+            Title = title;
+        }
+    }
+}
---
+++ PlanZen-Backend/PlanZen.API/Entities/Table.cs
@@ -0,0 +1,20 @@
+﻿namespace PlanZen.API.Entities
+{
+    public class Table
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        public ICollection<Column> Columns { get; set;} = new List<Column>();
+
+        public Table(string title)
+        { 
+            Title = title;
+        }
+    }
+}
